e2314de0be9f00bc2175821222cd5bcb6d600b99
Mariana Medeiros Ruddy Santos <mariana.ruddy@b2wdigital.com>
2018-06-08 18:15:41 -0300

    Finalizando menu lateral com API-git

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const URL = `https://randomuser.me/api/`;
+const URL = `https://api.github.com/users/marvin-ai/repos`;
 
 function createElement(tag) {
 	return document.createElement(tag);
@@ -11,16 +11,16 @@ function requerimento() {
   const request = fetch(URL);
 	request.then((response) => response.json()).then((response) => {
     console.log(response);
-    const tam =  response.results.length;
-    // console.log(tam)
+    const tam =  response.length;                                                               /*github*/
+    // const tam =  response.results.length;                                                    /*randomname*/
 		for (let i = 0; i < tam; i++) {      
 			let lista = document.getElementById('nav');
 			let li = createElement('li');
 			let a = createElement('a');
-			a.setAttribute('href', '/');
-      // a.innerHTML = response[i].name;
-      a.innerHTML = response.results[i].name.first;
-      console.log(a.innerHTML)
+			a.setAttribute('href', `./pagDeProjeto.html?name=${response[i].name}`)                    /*github*/
+			// a.setAttribute('href', `./pagDeProjeto.html?name=${response.results[i].name.first}`)   /*randomname*/
+      a.innerHTML = response[i].name;                                                           /*github*/
+      // a.innerHTML = response.results[i].name.first;                                          /*randomname*/
 			li.appendChild(a);
 			lista.appendChild(li);
 		};
