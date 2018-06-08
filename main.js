ff200261c0e9f656af13c8492fa2268ebfa0957b
Mariana Medeiros Ruddy Santos <mariana.ruddy@b2wdigital.com>
2018-06-08 16:57:48 -0300

    iniciando menu lateral com API-randomname funcionando

--- main.js
+++ main.js
@@ -1,33 +1,29 @@
-const URL = `https://api.github.com/users/marvin-ai/repos`
-     function consulta() {
-      
-      const request = fetch(URL)
-    
-      request
-        .then(response => response.json())
-        .then(response => {
-          console.log(response)
-          // document.getElementById("container").innerHTML=response.results[0].name.first
-        });
-    }
-    consulta();
-    // function pegarDadosProcessados(url) {
-    //   return baixarDados(url) // retorna uma Promise
-    //     .catch(e => {
-    //       return baixarDadosReservas(url) // retorna uma Promise
-    //     })
-    //     .then(v => {
-    //       return processarDadosNoWorker(v); // retorna uma Promise
-    //     });
-    // }
+const URL = `https://randomuser.me/api/`;
 
-    // listen (start app with node server.js) ======================================
-    // app.listen(8080, () => {
-    //   console.log("App listening on port 8080");
-    //   pegarDadosProcessados();
-    // });
-    
+function createElement(tag) {
+	return document.createElement(tag);
+}
+function getElelementById(id) {
+	return document.getElelementById(id);
+}
 
-
-
-  
+function requerimento() {
+  const request = fetch(URL);
+	request.then((response) => response.json()).then((response) => {
+    console.log(response);
+    const tam =  response.results.length;
+    // console.log(tam)
+		for (let i = 0; i < tam; i++) {      
+			let lista = document.getElementById('nav');
+			let li = createElement('li');
+			let a = createElement('a');
+			a.setAttribute('href', '/');
+      // a.innerHTML = response[i].name;
+      a.innerHTML = response.results[i].name.first;
+      console.log(a.innerHTML)
+			li.appendChild(a);
+			lista.appendChild(li);
+		};
+	});
+}
+requerimento();
